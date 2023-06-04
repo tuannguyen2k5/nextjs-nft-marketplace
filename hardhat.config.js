2f8191c920ecb986c6608e8d5f2d0efbd92fcd5b
padi-dev-tuannt <tuannt@paditech.com>
2023-06-04 20:35:26 +0700

    add package and config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [MUMBAI_PRIVATE_KEY]
+    }
+  },
 };
